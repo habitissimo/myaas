7dc18d9ec8c43f36ed37f633f44dbdb52579d7a4
Hugo Chinchilla Carbonell <hchinchilla@habitissimo.com>
2016-07-20 10:01:46 +0200

    master: add some logging

--- src/myaas/server.py
+++ src/myaas/server.py
@@ -1,6 +1,9 @@
+import logging
+import os
+
 from flask import Flask, Response, request, jsonify, abort
 
-from .settings import HOSTNAME
+from .settings import HOSTNAME, DEBUG
 from .utils.container import client
 from .utils.database import (get_myaas_containers, get_enabled_backend,
                              list_databases, list_database_templates)
@@ -9,6 +12,10 @@ from .backends.exceptions import (NonExistentDatabase, NonExistentTemplate,
 
 app = Flask(__name__)
 
+logger = logging
+logger.basicConfig(
+    format='%(asctime)s {:4} %(levelname)s: %(message)s'.format(os.getpid()),
+    level=logging.DEBUG if DEBUG else logging.WARNING)
 
 @app.route('/', methods=['get'])
 def hello_world():
@@ -39,10 +46,12 @@ def show_templates():
 
 @app.route('/db/<template>/<name>', methods=['get'])
 def inspect_database(template, name):
+    logger.debug("requested inspect DB for: \"{template}\" => \"{name}\"".format(**locals()))
     database = get_enabled_backend().Database
     try:
         db = database(client, template, name)
     except NonExistentDatabase:
+        logger.debug("database not found \"{template}\" => \"{name}\"".format(**locals()))
         abort(404)
 
     if request.args.get('all'):
@@ -61,9 +70,11 @@ def inspect_database(template, name):
 
 @app.route('/db/<template>/<name>', methods=['post'])
 def create_database(template, name):
+    logger.debug("requested create DB from \"{template}\" as \"{name}\"".format(**locals()))
     database_class = get_enabled_backend().Database
     try:
         db = database_class(client, template, name)
+        logger.warning("already exists \"{template}\" as \"{name}\"".format(**locals()))
         response = Response(status=304)  # not modified
         del response.headers['content-type']
         return response
@@ -73,13 +84,17 @@ def create_database(template, name):
     template_class = get_enabled_backend().Template
     try:
         template_db = template_class(client, template)
+        logger.debug("found template \"{template}\"".format(**locals()))
         db = template_db.clone(name)
+        logger.debug("starting database \"{template}\" => \"{name}\"".format(**locals()))
         db.start()
     except ImportInProgress:
+        logger.error("requested template \"{template}\" not available, import in progress".format(**locals()))
         response = jsonify(status="Database not available, content is being imported.")
         response.status_code = 500
         return response
     except NonExistentTemplate:
+        logger.error("requested template \"{template}\" not found".format(**locals()))
         response = jsonify(status="Template \"{0}\" does not exists.".format(template))
         response.status_code = 500
         return response
@@ -91,11 +106,14 @@ def create_database(template, name):
 
 @app.route('/db/<template>/<name>', methods=['delete'])
 def remove_database(template, name):
+    logger.debug("requested delete DB \"{template}\" => \"{name}\"".format(**locals()))
     database_class = get_enabled_backend().Database
     try:
         db = database_class(client, template, name)
         db.remove()
+        logger.debug("removed")
     except NonExistentDatabase:
+        logger.debug("database not found \"{template}\" => \"{name}\"".format(**locals()))
         abort(404)
 
     response = Response(status=204)
